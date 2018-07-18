3122b5f856dbe9b33a9dcd392f222a5d491f3621
ericoporto <ericoporto2008@gmail.com>
2018-07-18 07:29:34 -0300

    used depth 1 to get less files from remote #2 . moved remove source inside get-help-source . Removed clean script.

--- get-help-source.sh
+++ get-help-source.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-./clean-help-source.sh
+rm -rf source
 
-git clone https://github.com/ericoporto/agshelp.wiki.git source
+git clone --depth=1 --branch=master https://github.com/ericoporto/agshelp.wiki.git source
 
 cp conf.py source/conf.py
 mv source/Home.md source/index.md
