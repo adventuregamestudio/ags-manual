6197492970d4d7ec6716ac7a18aec6d055a05fc4
Morgan Willcock <morganwillcock@users.noreply.github.com>
2018-07-18 08:59:16 +0100

    render tables

--- conf.py
+++ conf.py
@@ -41,6 +41,7 @@ release = '3.4.1'
 # ones.
 extensions = [
     'sphinx.ext.githubpages',
+    'sphinx_markdown_tables'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
