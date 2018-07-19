e744f465a764871477eaff0aab423b8dbc9d44c8
Morgan Willcock <morganwillcock@users.noreply.github.com>
2018-07-19 23:35:16 +0100

    Clone with symlinks enabled, on Windows

--- get-help-source.sh
+++ get-help-source.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+dir="source"
+opts="--depth=1 --branch=master"
+url="https://github.com/ericoporto/agshelp.wiki.git"
+
 rm -rf source
-git clone --depth=1 --branch=master https://github.com/ericoporto/agshelp.wiki.git source
+uname -r | grep -q "Microsoft$" && opts="--config core.symlinks=true $opts"
+git clone $opts $url $dir
