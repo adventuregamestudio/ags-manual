1a3ec31f8b66f28b386136edb7cb714019cfc5a0
Morgan Willcock <morganwillcock@users.noreply.github.com>
2018-07-20 10:29:35 +0100

    Fix for POSIX and Msys

--- get-help-source.sh
+++ get-help-source.sh
@@ -1,9 +1,21 @@
-#!/bin/bash
+#!/bin/sh
 
 dir="source"
 opts="--depth=1 --branch=master"
 url="https://github.com/ericoporto/agshelp.wiki.git"
 
-rm -rf source
-uname -r | grep -q "Microsoft$" && opts="--config core.symlinks=true $opts"
+release=`uname -r`
+os=`uname -o`
+windows=0
+
+if [ ! "${release%Microsoft}" = "${release}" ]; then
+	# Windows Subsystem for Linux
+	windows=1
+elif [ "$os" = "Msys" ]; then
+	# Msys (subsystem from Git installer)
+	windows=1
+fi
+
+rm -rf "$dir"
+[ "$windows" -eq "1" ] && opts="--config core.symlinks=true $opts"
 git clone $opts $url $dir
