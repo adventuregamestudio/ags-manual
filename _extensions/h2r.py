e87066f7a6c9cea8655239ad486f5a2624afe773
Morgan Willcock <morganwillcock@users.noreply.github.com>
2018-09-11 19:42:50 +0100

    Add config option for forcing items as global functions

--- _extensions/h2r.py
+++ _extensions/h2r.py
@@ -43,7 +43,7 @@ def map_page(app, env, docnames):
                     subtitle = line[4:].strip()
 
                     if isinstance(script_object, str):
-                        logger.info('Script object: %s.%s' % (script_object, subtitle))
+                        logger.info('Found script object: %s.%s' % (script_object, subtitle))
                         changes[docname].append((offset, script_object, subtitle, True))
 
                         if subtitle in seen_script_attr:
@@ -52,7 +52,7 @@ def map_page(app, env, docnames):
                         else:
                             seen_script_attr.append(subtitle)
                     else:
-                        logger.info('Help section: %s, %s' % (title, subtitle))
+                        logger.info('Found help section: %s, %s' % (title, subtitle))
                         changes[docname].append((offset, title, subtitle, False))
 
                 offset += len(line)
@@ -64,9 +64,19 @@ def add_indices(app, docname, source):
     for insert_at, title, subtitle, is_script in changes[docname]:
         modified.append(source[0][offset:insert_at])
         offset = insert_at
+        force_global = False
+
+        try:
+            if subtitle in app.config.h2r_script_object_force_global[title]:
+                force_global = True
+        except KeyError:
+            pass
 
         if is_script:
-            modified.append('.. index::\n   single: %s.%s\n\n' % (title, subtitle))
+            if not force_global:
+                modified.append('.. index::\n   single: %s.%s\n\n' % (title, subtitle))
+            else:
+                logger.info('Script item \'%s\' forced as global' % (subtitle))
 
             if subtitle not in shared_script_attr:
                 modified.append('.. index::\n   single: %s\n\n' % (subtitle))
@@ -79,6 +89,7 @@ def add_indices(app, docname, source):
 
 def setup(app):
     app.add_config_value('h2r_script_object_from_header', r'(.*)', 'html')
+    app.add_config_value('h2r_script_object_force_global', {}, 'html')
     app.connect('env-before-read-docs', map_page)
     app.connect('source-read', add_indices)
     return {'version': sphinx.__display_version__, 'parallel_read_safe': True}
