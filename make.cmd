120d54b6e07191bb96b5afd7a7d6d1d068df2535
Morgan Willcock <morganwillcock@users.noreply.github.com>
2018-09-13 20:24:37 +0100

    Use batch file as entry point

---
+++ make.cmd
@@ -0,0 +1,60 @@
+@echo off
+setlocal enabledelayedexpansion
+
+if not defined SOURCEDIR set SOURCEDIR=source
+if not defined BUILDDIR set BUILDDIR=build
+if not defined SPHINXPROJ set SPHINXPROJ=AGSHelp
+if not defined SPHINXOPTS set SPHINXOPTS=-c .
+if not defined GITURL set GITURL=https://github.com/adventuregamestudio/ags-manual.wiki.git
+if not defined GITOPTS set GITOPTS=--depth=1 --branch=master
+
+if "%*" == "" (
+    echo No targets were specified!
+    echo.
+    for /f "tokens=*" %%t in ('findstr /r "^:" "%~nx0"') do (
+        set TARGET=%%t
+        set TARGET=!TARGET:~1!
+        if not "!TARGET!" == "end" echo    !TARGET!
+    )
+) else (
+    for %%t in (%*) do call :%%~t %%~t
+)
+
+goto end
+
+:clone
+if exist "%SOURCEDIR%" rd /s /q "%SOURCEDIR%"
+git clone %GITOPTS% %GITURL% %SOURCEDIR%
+exit /b
+
+:html
+:htmlhelp
+sphinx-build -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS%
+exit /b
+
+:chm
+move "%BUILDDIR%\htmlhelp\AGSHelpdoc.hhp" %TEMP% &&^
+findstr /v /c:"Binary TOC=No" /c:"Binary Index=No" %TEMP%\AGSHelpdoc.hhp > "%BUILDDIR%\htmlhelp\AGSHelpdoc.hhp"
+
+reg query HKLM\Hardware\Description\System\CentralProcessor\0 /c /f "x86" > nul:
+if %ERRORLEVEL% == 0 (
+    set HHC="%PROGRAMFILES%\HTML Help Workshop\hhc.exe"
+) else (
+    set HHC="%PROGRAMFILES(X86)%\HTML Help Workshop\hhc.exe"
+)
+
+%HHC% "%BUILDDIR%\htmlhelp\AGSHelpdoc.hhp"
+move "%BUILDDIR%\htmlhelp\AGSHelpdoc.chm" ags-help.chm
+exit /b
+
+:clean
+for /r %%d in (*.gitignore) do (
+    for /f "tokens=*" %%c in (%%d) do (
+        echo Cleaning %%~pd%%c
+        2>nul rd /s /q "%%~pd%%c" || del "%%~pd%%c" /q /f
+    )
+)
+exit /b
+
+:end
+endlocal
