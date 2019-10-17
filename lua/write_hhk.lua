29dc4e2556b9e37e646a4f58a140f34dbe5f4423
Morgan Willcock <morganwillcock@users.noreply.github.com>
2019-10-18 00:02:23 +0100

    Write page subtitles in a child list for CHM index

--- lua/write_hhk.lua
+++ lua/write_hhk.lua
@@ -6,6 +6,7 @@ local agsman = require('agsman')
 local meta = PANDOC_DOCUMENT.meta
 local stringify = (require 'pandoc.utils').stringify
 local indices = {}
+local sections = {}
 
 function Doc(body, metadata, variables)
   local buffer = {}
@@ -16,10 +17,23 @@ function Doc(body, metadata, variables)
 
   -- get all of the heading info into a table
   for k, v in pairs(meta) do
+    local title = stringify(v.title)
     if v.index then
       for itemtype, item in pairs(v.index) do
         for name, id in pairs(item) do
-          indices[name] = k .. '.html#' .. stringify(id)
+          local pagelink = k .. '.html#' .. stringify(id)
+
+          if itemtype == 'script' or name == title then
+            -- if this is a script item or a page title then add the link at the root level
+            indices[name] = pagelink
+          else
+            -- if this is not a script item and not the title, add as a subitem under the title
+            if sections[title] ~= nil then
+              table.insert(sections[title], { [name] = pagelink })
+            else
+              sections[title] = {{ [name] = pagelink }}
+            end
+          end
         end
       end
     end
@@ -30,11 +44,25 @@ function Doc(body, metadata, variables)
   end
 
   -- sort the table and write it
-  for name, id in agsman.pairs_by_keys(indices, order) do
-    table.insert(buffer, string.format(format, name, id))
+  for name, pagelink in agsman.pairs_by_keys(indices, order) do
+    -- add script object or page header
+    table.insert(buffer, string.format(format, name, pagelink))
+
+    -- add page subsections as a child list
+    if sections[name] ~= nil then
+      table.insert(buffer, '<UL>')
+
+      for _, section in ipairs(sections[name]) do
+        for sectionname, sectionlink in pairs(section) do
+          table.insert(buffer, string.format(format, sectionname, sectionlink))
+        end
+      end
+
+      table.insert(buffer, '</UL>')
+    end
   end
 
-  return table.concat(buffer, '\n')
+  return '<UL>' .. table.concat(buffer, '\n') .. '</UL>'
 end
 
 local meta = {}
